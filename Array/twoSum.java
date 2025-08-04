00fe52dd31bef321a902f9e7c0b7e4b011dc7a70
Danish Tyagi <tyagidanish8549@gmail.com>
2025-08-05 04:15:15 +0530

    Added max subarray sum solution in array folder

---
+++ Array/twoSum.java
@@ -0,0 +1,17 @@
+//Leetcode Problem: https://leetcode.com/problems/two-sum/
+//Time Complexity: O(n^2)
+//Space Compexity: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+
+            }
+        }
+        return new int[] {};
+    }
+}
